b0eea8667b1065bca3d76408f42578e344df69d8
Abhinav Jain <abhinavjain10905@gmail.com>
2026-02-04 20:03:22 +0530

    Add files via upload

---
+++ Withdraw_amount.py
@@ -0,0 +1,8 @@
+def withdraw_amount(dict):
+    a=int(input("Enter your account number:"))
+    if a not in dict:
+        print("Account not found")
+    else:
+        b=float(input("Enter amount to withdraw:"))
+        dict[a][2]-=b
+        print("Amount withdrawl successful")
